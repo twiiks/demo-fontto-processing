be77ab7048b661c89f1dcdfbb1fce337beba71a4
장상현 <jexjang92@gmail.com>
2017-10-18 23:15:13 +0900

    init utils.py
    ---
    Redmine Issue Progress / ID / Time :  60% #79 @30m
    ---
    What :
    make function deal_opt
    
    Why :
    opt를 한 번만 부르게
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

---
+++ utils.py
@@ -0,0 +1,11 @@
+from options.test_options import TestOptions
+
+
+# this function should be called in written all
+def deal_opt():
+    opt = TestOptions().parse()
+    opt.nThreads = 1  # test code only supports nThreads = 1
+    opt.batchSize = 1  # test code only supports batchSize = 1
+    opt.serial_batches = True  # no shuffle
+    opt.no_flip = True  # no flip
+    return opt
