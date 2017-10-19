41e3c0d2ee96f213a4650adfdc5d6640473a7a45
장상현 <jexjang92@gmail.com>
2017-10-19 17:20:21 +0900

    merge one2class and written2all
    ---
    Redmine Issue Progress / ID / Time :  100% #81 @10m
    ---
    What :
    one2class 함수를 written2all 파일에 합침
    
    Why :
    더 나은 구조를 위해
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- written2all.py
+++ written2all.py
@@ -1,6 +1,46 @@
+#one2class
+import sys
+sys.path.append("./generator/")
+
+import os
 from PIL import Image
+from generator import generator
+from PIL2Tensor import to_tensor
+
+#written2all
 from utils import deal_opt
-from one2class import one2class
+
+
+def one2class(unicode_input, image_input, opt):
+    path_class = "./pths/%s/" % (unicode_input)
+    if not os.path.isdir(path_class):
+        return {}
+
+    images_output = {}
+
+    # change image type to tensor float
+    image_input_tensor = to_tensor(image_input)
+    image_input_tensor = image_input_tensor.unsqueeze(0)
+
+    # if pth exist, generate another character using pth
+    dirs = os.listdir(path_class)
+    for dir in dirs:
+        if dir.split('_')[0] == unicode_input:
+            files = os.listdir("%s/%s/" % (path_class, dir))
+            for filename in files:
+                ext = os.path.splitext(filename)[-1]
+                if ext == '.pth':
+                    unicode_output = dir.split('_')[1].split('.')[-1]
+                    # log
+                    print("now dealing with :", unicode_output)
+                    path_pth = os.path.abspath("%s/%s/%s" % (path_class, dir,
+                                                             filename))
+                    image_gen = generator(image_input_tensor, opt, path_pth)
+                    images_output[unicode_output] = image_gen
+                    break
+
+    return images_output
+
 
 def written2all(unicode_image):
     opt = deal_opt()
