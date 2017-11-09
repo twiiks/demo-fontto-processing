a925eb85b9bdc2582b183db6d0cbf8010fe44c8e
장상현 <jexjang92@gmail.com>
2017-11-09 15:33:44 +0900

    사용되지 않는 함수 및 파일 삭제
    ---
    Redmine Issue Progress / ID / Time :  [ ]% #[ ] @[ ]
    ---
    What :
    - train, cycle 삭제
    
    Why :
    경량화
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- generator/models/models.py
+++ generator/models/models.py
@@ -1,20 +1,6 @@
+from .test_model import TestModel
 
 def create_model(opt, path_pth):
-    model = None
-    # print(opt.model)
-    if opt.model == 'cycle_gan':
-        assert(opt.dataset_mode == 'unaligned')
-        from .cycle_gan_model import CycleGANModel
-        model = CycleGANModel()
-    elif opt.model == 'pix2pix':
-        assert(opt.dataset_mode == 'aligned')
-        from .pix2pix_model import Pix2PixModel
-        model = Pix2PixModel()
-    elif opt.model == 'test':
-        assert(opt.dataset_mode == 'single')
-        from .test_model import TestModel
-        model = TestModel()
-    else:
-        raise ValueError("Model [%s] not recognized." % opt.model)
+    model = TestModel()
     model.initialize(opt, path_pth)
     return model
